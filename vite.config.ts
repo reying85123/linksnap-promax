36b240d9a4786adcb4a99c687505eef531d351ea
LeoHong <Leo266470@gmail.com>
2026-03-02 01:03:06 +0800

    feat: github page setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/linksnap-promax/',
   plugins: [vue(), tailwindcss()],
 })
